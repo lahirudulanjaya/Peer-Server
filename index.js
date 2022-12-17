b1b4012f53115fdfc98d6f6e14e5450742a7b328
lahiru <laahirudulanjaya@gmail.com>
2022-12-18 00:53:48 +0530

    change the implementation

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const { PeerServer } = require('peer');
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const cors = require('cors');
 
-const peerServer = PeerServer({ port: 9000, path: '' })
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+app.use('', peerServer);
+
+peerServer.on('connection', (client) => { console.log('client connected');});
+peerServer.on('disconnect', (client) => { console.log('client disconnected');});
